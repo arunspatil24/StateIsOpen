3dca213bb86caf179b0ea3e1f4ce6ad41b082fb9
shashikantx <shashikantrudrawadi@gmail.com>
2020-06-08 17:00:34 +0530

    updated search endpoint

--- index.js
+++ index.js
@@ -1,169 +1,68 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-app.use(express.json());
-var bodyParser = require('body-parser');
+var bodyParser = require("body-parser");
 app.use(bodyParser.json()); // support json encoded bodies
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-var cors = require('cors')
-const { Client } = require('pg');
-const connectionString = 'postgres://postgres:88929%40run@localhost:5432/KYIsOpen';
-const client = new Client({
-    connectionString: connectionString
+var cors = require("cors");
+const { Pool } = require("pg");
+const connectionString = "postgres://postgres:srr123@localhost:5432/ky_is_open";
+const pool = new Pool({
+  connectionString: connectionString,
 });
-client.connect();
-app.use(cors())
-app.get('/getid', (req, res) => { 
-    console.log(req.query.id)
-  client.query('SELECT * FROM public."Contacts"', function (err, result) {
-    if (err) {
+pool.connect();
+app.use(cors());
+
+
+app.post("/open/search", (req, res) => {
+  const name = req.body.name;
+  const page = (req.query.page < 0 ? 0 : req.query.page) || 0;
+  const size = (req.query.size < 1 ? 20 : req.query.size) || 20;
+  const queryString = 
+
+  'SELECT' +
+  '"StateBusinesses"."Id",'+
+  '"StateBusinesses"."County",'+
+  '"StateBusinesses"."Name", '+
+  '"StateBusinesses"."Address",'+
+  '"StateBusinesses"."City",'+
+  '"StateBusinesses"."PostalCode",'+
+  '"StateBusinesses"."AltPhone",'+
+  '"StateBusinesses"."Website",'+
+  '"StateBusinesses"."Sales",'+
+  '"StateBusinesses"."Employees",'+
+  '"StateBusinesses"."SicCode",'+
+  '"StateBusinesses"."Industry",'+
+  '"StateBusinesses"."EmailDomain",'+
+  'json_agg(' +
+      'json_build_object(' +
+          '\'Id\', "Contacts"."Id",'+
+          '\'Name\', "Contacts"."Name",'+
+          '\'Title\', "Contacts"."Title",'+
+          '\'Phone\', "Contacts"."Phone",'+
+          '\'Email\', "Contacts"."Email" '+
+      ')' +
+  ')' +
+  'as "Contacts"' +
+  'FROM public."StateBusinesses" Inner Join "Contacts" On public."StateBusinesses"."Id" = "Contacts"."StateBusinessId"' +
+  'Where "StateBusinesses"."Name" Like $1 Group By "StateBusinesses"."Id"';
+
+  if (name != undefined && name != null) {
+    const query = {
+      text: queryString +' LIMIT $2 OFFSET $3',
+      values: [
+          name + '%',
+          size,
+          page * size
+      ]
+    };
+    return pool.query(query, function (err, result) {
+      if (err) {
         console.log(err);
         res.status(400).send(err);
-    }
-    res.status(200).send(result.rows);
+      }
+      res.send(result.rows);
     });
-// res.status(200).send("Hello")
+  }
 });
-
-
-app.post('/open/search', (req, res) => { 
-    console.log(req.body);
-    // const name=req.body.name;
-    // if(req.body.name != undefined || req.body.name != null){
-    //     const query={text:'SELECT * FROM public."StateBusinesses" where "Name" like $1 Limit 20',values:["%"+name]}
-    //     client.query(query, function (err, result) {
-    //       if (err) {
-    //           console.log(err);
-    //           res.status(400).send(err);
-    //       }
-    //     //   res.rows.forEach(element => {
-    //     //       element["name"] = element.Name;
-    //     //   });
-    //       res.status(200).send(result.rows);
-    // }); 
-    // }
-    res.status(200).send({name:""});
-}); 
-app.post('/open/businesstype', (req, res) => { 
-    console.log(req.body);
-    // const name=req.body.name;
-    // if(req.body.name != undefined || req.body.name != null){
-    //     const query={text:'SELECT * FROM public."StateBusinesses" where "Name" like $1 Limit 20',values:["%"+name]}
-    //     client.query(query, function (err, result) {
-    //       if (err) {
-    //           console.log(err);
-    //           res.status(400).send(err);
-    //       }
-    //     //   res.rows.forEach(element => {
-    //     //       element["name"] = element.Name;
-    //     //   });
-    //       res.status(200).send(result.rows);
-    // }); 
-    // }
-    res.status(200).send({name:""});
-}); 
-
-
-app.post('/open/state', (req, res) => { 
-    console.log(req.body);
-    // const name=req.body.name;
-    // if(req.body.name != undefined || req.body.name != null){
-    //     const query={text:'SELECT * FROM public."StateBusinesses" where "Name" like $1 Limit 20',values:["%"+name]}
-    //     client.query(query, function (err, result) {
-    //       if (err) {
-    //           console.log(err);
-    //           res.status(400).send(err);
-    //       }
-    //     //   res.rows.forEach(element => {
-    //     //       element["name"] = element.Name;
-    //     //   });
-    //       res.status(200).send(result.rows);
-    // }); 
-    // }
-    res.status(200).send({name:""});
-}); 
-
-
-app.post('/getid', (req, res) => { 
-    console.log(req.body);
-    // const name=req.body.name;
-    // if(req.body.name != undefined || req.body.name != null){
-    //     const query={text:'SELECT * FROM public."StateBusinesses" where "Name" like $1 Limit 20',values:["%"+name]}
-    //     client.query(query, function (err, result) {
-    //       if (err) {
-    //           console.log(err);
-    //           res.status(400).send(err);
-    //       }
-    //     //   res.rows.forEach(element => {
-    //     //       element["name"] = element.Name;
-    //     //   });
-    //       res.status(200).send(result.rows);
-    // }); 
-    // }
-    res.status(200).send({name:""});
-}); 
-
-app.post('/open', (req, res) => { 
-    console.log(req.body);
-    // const name=req.body.name;
-    // if(req.body.name != undefined || req.body.name != null){
-    //     const query={text:'SELECT * FROM public."StateBusinesses" where "Name" like $1 Limit 20',values:["%"+name]}
-    //     client.query(query, function (err, result) {
-    //       if (err) {
-    //           console.log(err);
-    //           res.status(400).send(err);
-    //       }
-    //     //   res.rows.forEach(element => {
-    //     //       element["name"] = element.Name;
-    //     //   });
-    //       res.status(200).send(result.rows);
-    // }); 
-    // }
-    res.status(200).send({name:""});
-}); 
-app.get('/open', (req, res) => { 
-    console.log(req.body);
-    // const name=req.body.name;
-    // if(req.body.name != undefined || req.body.name != null){
-    //     const query={text:'SELECT * FROM public."StateBusinesses" where "Name" like $1 Limit 20',values:["%"+name]}
-    //     client.query(query, function (err, result) {
-    //       if (err) {
-    //           console.log(err);
-    //           res.status(400).send(err);
-    //       }
-    //     //   res.rows.forEach(element => {
-    //     //       element["name"] = element.Name;
-    //     //   });
-    //       res.status(200).send(result.rows);
-    // }); 
-    // }
-    res.status(200).send({name:""});
-}); 
-app.get('/open/activate', (req, res) => { 
-    console.log(req.body);
-    // const name=req.body.name;
-    // if(req.body.name != undefined || req.body.name != null){
-    //     const query={text:'SELECT * FROM public."StateBusinesses" where "Name" like $1 Limit 20',values:["%"+name]}
-    //     client.query(query, function (err, result) {
-    //       if (err) {
-    //           console.log(err);
-    //           res.status(400).send(err);
-    //       }
-    //     //   res.rows.forEach(element => {
-    //     //       element["name"] = element.Name;
-    //     //   });
-    //       res.status(200).send(result.rows);
-    // }); 
-    // }
-    res.status(200).send({name:""});
-}); 
-// app.post('/open/businesstype', (req, res) => { 
-//     client.query('SELECT * FROM public."Contacts"', function (err, result) {
-//       if (err) {
-//           console.log(err);
-//           res.status(400).send(err);
-//       }
-//       res.status(200).send(result.rows);
-//       });
-//   });
-app.listen(4000, () => console.log('listening on port 4000'));
+app.listen(4000, () => console.log("listening on port 4000"));
