3e58da8e076232460142aa0088bd0c228cb4ba69
shashikantx <shashikantrudrawadi@gmail.com>
2020-06-10 16:10:34 +0530

    added open api endpoint

--- index.js
+++ index.js
@@ -6,54 +6,51 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 var cors = require("cors");
 const { Pool } = require("pg");
-const connectionString = "postgres://masteradmin:D2jjY6tQT2XfNzrhH4Ko@dev-aurora-db-cluster.cluster-clhbqhjqo4mz.us-east-1.rds.amazonaws.com:5432/kybusinessdata";
+const { json } = require("body-parser");
+const connectionString = "postgres://postgres:srr123@localhost:5432/ky_is_open";
 const pool = new Pool({
   connectionString: connectionString,
 });
 pool.connect();
 app.use(cors());
 
-
 app.post("/open/search", (req, res) => {
   const name = req.body.name;
   const page = (req.query.page < 0 ? 0 : req.query.page) || 0;
   const size = (req.query.size < 1 ? 20 : req.query.size) || 20;
-  const queryString = 
-  'SELECT' +
-  '"StateBusinesses"."Id",'+
-  '"StateBusinesses"."County",'+
-  '"StateBusinesses"."Name", '+
-  '"StateBusinesses"."Address",'+
-  '"StateBusinesses"."City",'+
-  '"StateBusinesses"."PostalCode",'+
-  '"StateBusinesses"."AltPhone",'+
-  '"StateBusinesses"."Website",'+
-  '"StateBusinesses"."Sales",'+
-  '"StateBusinesses"."Employees",'+
-  '"StateBusinesses"."SicCode",'+
-  '"StateBusinesses"."Industry",'+
-  '"StateBusinesses"."EmailDomain",'+
-  'json_agg(' +
-      'json_build_object(' +
-          '\'Id\', "Contacts"."Id",'+
-          '\'Name\', "Contacts"."Name",'+
-          '\'Title\', "Contacts"."Title",'+
-          '\'Phone\', "Contacts"."Phone",'+
-          '\'Email\', "Contacts"."Email" '+
-      ')' +
-  ')' +
-  'as "Contacts"' +
-  'FROM public."StateBusinesses" Inner Join "Contacts" On public."StateBusinesses"."Id" = "Contacts"."StateBusinessId"' +
-  'Where "StateBusinesses"."Name" Like $1 Group By "StateBusinesses"."Id"';
+  const queryString =
+    "SELECT" +
+    '"StateBusinesses"."Id",' +
+    '"StateBusinesses"."County",' +
+    '"StateBusinesses"."Name", ' +
+    '"StateBusinesses"."Address",' +
+    '"StateBusinesses"."City",' +
+    '"StateBusinesses"."PostalCode",' +
+    '"StateBusinesses"."AltPhone",' +
+    '"StateBusinesses"."Website",' +
+    '"StateBusinesses"."Sales",' +
+    '"StateBusinesses"."Employees",' +
+    '"StateBusinesses"."SicCode",' +
+    '"StateBusinesses"."Industry",' +
+    '"StateBusinesses"."EmailDomain",' +
+    "json_agg(" +
+    "json_build_object(" +
+    '\'Id\', "Contacts"."Id",' +
+    '\'Name\', "Contacts"."Name",' +
+    '\'Title\', "Contacts"."Title",' +
+    '\'Phone\', "Contacts"."Phone",' +
+    '\'Email\', "Contacts"."Email" ' +
+    ")" +
+    ")" +
+    'as "Contacts"' +
+    'FROM public."StateBusinesses" Inner Join "Contacts" On public."StateBusinesses"."Id" = "Contacts"."StateBusinessId"' +
+    'Where "StateBusinesses"."Name" Like $1 Group By "StateBusinesses"."Id"';
 
   if (name != undefined && name != null) {
     const query = {
-      text: 'Select * From public."StateBusinesses" Where Lower("StateBusinesses"."name") Like Lower($1) LIMIT $2 OFFSET $3',
-      values: [
-          name + '%',
-          size,
-          page * size
-      ]
+      text:
+        'Select * From public."StateBusinesses" Where Lower("StateBusinesses"."name") Like Lower($1) LIMIT $2 OFFSET $3',
+      values: [name + "%", size, page * size],
     };
     return pool.query(query, function (err, result) {
       if (err) {
@@ -61,15 +58,14 @@ app.post("/open/search", (req, res) => {
         res.status(400).send(err);
       }
       var results = Array();
-      result.rows.forEach(element => {
-        results.push({"_source":element});
+      result.rows.forEach((element) => {
+        results.push({ _source: element });
       });
       res.send(results);
     });
   }
   res.send([]);
 });
-
 
 app.post("/open/businesstype", (req, res) => {
   console.log(req.body);
@@ -79,11 +75,7 @@ app.post("/open/businesstype", (req, res) => {
   if (type != undefined && type != null) {
     const query = {
       text: 'Select * From public."SICCodes" Where Lower("SICCodes"."industry") Like Lower($1) LIMIT $2 OFFSET $3',
-      values: [
-          '%' + type + '%',
-          size,
-          page * size
-      ]
+      values: ["%" + type + "%", size, page * size],
     };
     return pool.query(query, function (err, result) {
       if (err) {
@@ -91,13 +83,30 @@ app.post("/open/businesstype", (req, res) => {
         res.status(400).send(err);
       }
       var results = Array();
-      result.rows.forEach(element => {
-        results.push({"_source":element});
+      result.rows.forEach((element) => {
+        results.push({ _source: element });
       });
       res.send(results);
     });
   }
   res.send([]);
+});
+
+app.post("/open", (req, res) => {
+  const payload = req.body.doc;
+  //console.log(req.body.doc);
+  const query = {
+    text: 'UPDATE public."StateBusinesses" Set formdata = $1 where "StateBusinesses"."id" = $2',
+    values: [JSON.stringify(payload), payload.id],
+  };
+  return pool.query(query, function (err, result) {
+    if (err) {
+      console.log(err);
+      res.status(400).send(err);
+    }
+    res.status(200).send(result);
+    return;
+  });
 });
 
 app.listen(4000, () => console.log("listening on port 4000"));
